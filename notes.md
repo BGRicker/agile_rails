6fd36feb2d5b58e4ea1747001df3d9429788fd65
Ben Ricker <bgricker@gmail.com>
2015-11-15 23:06:37 -0500

    test coverage

---
+++ notes.md
@@ -0,0 +1,3 @@
+assert if we create product without attributes then it should contain errors based on our validations
+  assert product.errors[:title].any?  # assert will pass if statement is true, if there are errors then this will be true.
+there will be errors because we have validations to check presence of title
--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,7 +1,31 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "product attributes must not be empty" do
+    product = Product.new
+    assert product.invalid?
+    assert product.errors[:title].any?
+    assert product.errors[:description].any?
+    assert product.errors[:price].any?
+    assert product.errors[:image_url].any?
+  end
+
+  test "product price must be positive" do
+    product = Product.new(:title        => "My Book Title",     # create new product
+                          :description  => "yyy",
+                          :image_url    => "zzz.jpg")
+    product.price = -1                                          # set price to negative one, validations should catch this
+    assert product.invalid?                                     # check if that's invalid, if it's invalid then this passes
+    assert_equal "must be greater than or equal to 0.01",
+      product.errors[:price].join('; ')
+
+    product.price = 0                                           # set price to zero, validations should catch this
+    assert product.invalid?                                     # check if that's invalid, if it's invalid then this passes
+    assert_equal "must be greater than or equal to 0.01",
+      product.errors[:price].join('; ')
+
+    product.price = 1                                           # set price to 1, allowed by validations
+    assert product.valid?                                       # this price should be valid
+  end
+
 end
